c10fdf647fe5e7aaaaa64eabcfd92837928a9c85
Tanner Shaw <traumaaa@gmail.com>
2024-01-24 16:23:27 -0800

    refactor(header) update header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import React, { ReactElement } from 'react';
+import classNames from 'classnames';
+import './index.scss';
 
 export default function Header(): ReactElement {
 
@@ -7,15 +9,14 @@ export default function Header(): ReactElement {
     <header className="flex flex-row items-center justify-between h-16 px-4 bg-gray-800 text-white">
         <div className="text-xl font-bold">TLSN Explorer</div>
       <div className="flex flex-row items-center gap-4">
-        <a href="https://tlsnotary.org/" className="
-          flex flex-row items-center justify-center
-          w-40 h-10 rounded button"
+        <a href="https://tlsnotary.org/"
+         className="flex flex-row items-center justify-center w-40 h-10 rounded button"
            target="_blank">
           About TLSNotary
         </a>
         <a href="https://github.com/tlsnotary/explorer" className="
           flex flex-row items-center justify-center
-          w-16 h-10 rounded button
+          w-16 h-10 button rounded
         " target="_blank">
           Source
         </a>
