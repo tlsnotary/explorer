144c896c299ad324116b7e5b0da197d979bc5c79
tsukino <0xtsukino@gmail.com>
2024-02-20 23:40:45 +0800

    feat(server): add ipfs upload and retrieval

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,51 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
-import { addBytes } from './services/ipfs';
+import stream from 'stream';
+
+import { addBytes, getCID } from './services/ipfs';
 
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+  res.setHeader('Cross-origin-Opener-Policy','same-origin');
 
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+});
 app.use(express.static('build/ui'));
 app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: '/tmp/',
   limits: { fileSize: 1024 * 1024 }, // 1mb file limit
 }));
 
 app.post('/upload', async (req, res) => {
   for (const file of Object.values(req.files!)) {
-    console.log(file);
     // @ts-ignore
-    const cid = await addBytes(file.data);
+    const data = file.data;
+    const cid = await addBytes(data);
     res.send(cid.toString());
+
+    return;
   }
 
   res.status(400).send({ error: true, message: 'request is missing file' });
+});
+
+app.get('/ipfs/:cid', async (req, res) => {
+  const cid = req.params.cid;
+  const file = await getCID(req.params.cid);
+  const readStream = new stream.PassThrough();
+  readStream.end(Buffer.from(file));
+  res.set('Content-Type', 'application/octet-stream');
+  res.set('Content-Disposition', `attachment; filename=${cid}.json`);
+  readStream.pipe(res);
 });
 
 app.listen(port, () => {
