056396fc9b45fc081e2cba993998efb98c5c7c56
tsukino <0xtsukino@gmail.com>
2024-02-20 23:40:45 +0800

    feat(server): add ipfs upload and retrieval

--- server/services/ipfs.ts
+++ server/services/ipfs.ts
@@ -1,5 +1,7 @@
 import { createHelia, HeliaLibp2p } from 'helia';
 import { unixfs } from '@helia/unixfs';
+import { CID } from 'multiformats/cid'
+
 
 let node: HeliaLibp2p | null = null;
 
@@ -15,4 +17,11 @@ export async function addBytes(buf: Buffer) {
   const fs = unixfs(ipfs);
   const cid = await fs.addBytes(buf);
   return cid;
+}
+
+export async function getCID(hash: string) {
+  const ipfs = await getIPFS();
+  const cid = CID.parse(hash);
+  const file = await ipfs.blockstore.get(cid);
+  return file;
 }
