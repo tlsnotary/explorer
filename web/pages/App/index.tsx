6b58904a65c35157219c637f32346015fb96143c
Codetrauma <jtannershaw@gmail.com>
2025-03-03 11:33:14 -0800

    feat: adding matomo tracking to explorer

--- web/pages/App/index.tsx
+++ web/pages/App/index.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../../components/Header';
 import SharedProof from '../../components/SharedProof';
 import FileDrop from '../FileDrop';
+import MatomoTracking from '../../components/MatomoTracking';
 import './index.scss';
 
 export default function App(): ReactElement {
   return (
     <div className="app flex flex-col gap-4">
       <Header />
+      <MatomoTracking />
       <Routes>
         <Route path="/" element={<FileDrop />} />
         <Route path="/ipfs/:cid" element={<SharedProof />} />
